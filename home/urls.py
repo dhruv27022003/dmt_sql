2e05f1126a6d2c1ca47bdc93ea9321289eb14d57
RISHABH DADHEECH <rishabhdadheech@RISHABHs-MacBook-Air.local>
2023-07-26 23:15:21 +0530

    final commit

--- home/urls.py
+++ home/urls.py
@@ -3,14 +3,24 @@ from django.urls import path ,include
 from home import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.logi, name="logi"),
     path('create', views.create, name="create"),
+     path('/index', views.index, name="index"),
+     path('index', views.index, name="index"),
+     path('cart', views.cart, name="cart"),
     path('about', views.about, name="about"),
     path('thanks', views.thanks, name="thanks"),
     path('thanks1', views.thanks1, name="thanks1"),
     path('thanks2', views.thanks2, name="thanks2"),
+    path('profile', views.pro, name="pro"),
     path('login', views.logi, name="logi"),
+    path('cartin', views.cartin, name="cartin"),
+      path('login', views.logi, name="logi"),
+    path('display', views.display, name="display"),
     path('logout', views.logou, name="logou"),
+    path('remove', views.remove, name="remove"),
+    
+    #  path('addp', views.addp, name="addp"),
     #  path('thanks', views.thanks, name="thanks"),
     # path('logout', views.logou, name="logou"),
 
--- home/views.py
+++ home/views.py
@@ -1,10 +1,33 @@
 from collections import UserDict
+from pdb import post_mortem
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import logout,login
 from django.contrib import messages
+import mysql.connector as sql
+cu=""
+un=""
+pno=""
+ps=""
+em=""
+cun=""
+pss=""
+pr="0"
+ui="yooo"
+d1={
+   'y1':'roo',
+   'mail':'roo',
+   'phn':'roo'
+}
+dd={}
+ds="LOGIN"
+m2="."
+m3="."
+co=1
+m = sql.connect(host="localhost",user="root",password="26papapapa",database="dmt")
+curs=m.cursor()
 # Create your views here.
 
 
@@ -22,14 +45,30 @@ def thanks(requests):
 
 def create(request):
      if request.method=="POST":
-        username=request.POST.get('username')
-        password=request.POST.get('password')
-        lastname =request.POST.get('NUMBER')
-        email =request.POST.get('email')
+      #   username=request.POST.get('username')
+      #   password=request.POST.get('password')
+      #   lastname =request.POST.get('NUMBER')
+      #   email =request.POST.get('email')
         
-      #   print(username,password,)
-        user = User.objects.create_user(username=username,email=email,last_name =lastname, password=password)
-        user.save()
+      #  #   print(username,password,)
+      #   user = User.objects.create_user(username=username,email=email,last_name =lastname, password=password)
+      #   user.save()
+        global un,ps,em,pno
+      #   m = sql.connect(host="localhost",user="root",password="26papapapa",database="dmt")
+        cursor=m.cursor()
+
+        d=request.POST
+        for key,value in d.items():
+           if key == "username" :
+              un = value
+           if key == "email" :
+              em = value
+           if key == "NUMBER" :
+              pno = value
+           if key == "password" :
+              ps = value         
+        cursor.execute("insert into users values('{}','{}','{}',{})".format(un,em,pno,ps))
+        m.commit()
         messages.success(request,'YOUR ACCOUNT IS CREATED PLEASE LOGIN NOW')
         return render(request,'login.html')
 
@@ -37,34 +76,80 @@ def create(request):
 
 
 def index(request):
-    print(request.user)
-    if request.user.is_anonymous:
-     return redirect("/login")
+   #  print(request.user)
+   #  if request.user.is_anonymous:
+   #   return redirect("/login")
+    co=2
     return render(request, 'index.html')
     #  return HttpResponse('<h1>Page not')
  
 def logi(request):
-     if request.method == "POST":
+   global cun,pss,d1,ds,m2,m3,ui,co
+
+   if request.method == "POST":
           
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+         #  m = sql.connect(host="localhost",user="root",password="26papapapa",database="dmt")
+         #  curs=m.cursor()
+          d=request.POST
+          for key,value in d.items():
 
-     #    print(usern,passw,123)
-        user = authenticate(username= username, password=password)
-        if user is not None:
-             # A backend authenticated the credentials
-             print(password,12344221)
-             login(request,user)
-             
-             return  redirect("/")
+           if key == "email" :
+              cun = value
+           if key == "password" :
+              pss = value         
+          d = "select * from users where mail = '{}' and pass = '{}'".format(cun,pss)
+          curs.execute(d)
+          t = tuple(curs.fetchall())
+         #  curs.execute("insert into users values ('yo','1@11','123','221')")
+         #  m.commit()
+          if t!=():
+           dd = {
+               'yo' : list(t)[0][0]
+               
+           }
+           d1 = {
+               'yo' : list(t)[0][0],
+               'mail': list(t)[0][1],
+               'phn': list(t)[0][2]
+
+               
+           }
+           ds = list(t)[0][0]
+           m2 = list(t)[0][1]
+           m3 = list(t)[0][2]
+           ui = ds
+           return redirect("/display")
+           return render(request, 'index.html',dd)
+          else :
+             dd = {
+                'y1': "ENTER CORRECT DETAILS"
+             }
+             return render(request,'login.html',dd)
      
-        else:
-              
-              messages.warning(request,'INVALID')
-              return render(request, 'login.html')
-         # No backend authenticated the credentials
+   return render(request,'login.html')
+    
+    
+   #  return render(request, 'login.html')     
+      #   t= tuple(curs.fetchall())
+      #   if t!=():
+      #      return render(request, 'index.html')
 
-     return render(request, 'login.html')
+
+
+
+      #   if user is not None:
+      #        # A backend authenticated the credentials
+      #        print(password,12344221)
+      #        login(request,user)
+             
+      #        return  redirect("/")
+     
+      #   else:
+              
+      #         messages.warning(request,'INVALID')
+      #         return render(request, 'login.html')
+         # No backend authenticated the credentials
+   #   return render(request, 'login.html')
 
 
 def logou(request):
@@ -74,4 +159,110 @@ def logou(request):
 
 def cre(request):
    return redirect("create.html")
+
+def pro(request):
+   global d1
+   d1={
+      'y1':ds,
+      'mail':m2,
+      'phn':m3
+   }
+   return render(request,'profile.html',d1)
+
+
+
+
+
+
+
+def cart(request):
    
+    c = "show tables like '{}'".format(ui)
+    curs.execute(c)
+    f=tuple(curs.fetchall())
+   
+    if f!=():
+          return redirect("/cartin")
+          return render(request,'index.html')
+    else :   
+      d= "create table {} (id varchar(10),pr varchar(10)) ".format(ui)
+      curs.execute(d)
+      m.commit()
+      # de={ 'uu': f }
+      return redirect("/cartin")
+      return render(request,'index.html')
+    return redirect("/login")
+
+
+
+
+
+def cartin(request):
+           global cu,pr
+           m = sql.connect(host="localhost",user="root",password="26papapapa",database="dmt")
+           curs=m.cursor()
+           
+           if request.method=="POST":
+               d=request.POST
+               for key,value in d.items():
+                if key == "tt" :
+                 cu = value
+                if key == "pp" :
+                 pr = value
+               curs.execute("insert into {} values('{}','{}')".format(ui,cu,pr))
+               m.commit() 
+
+           
+           d = "select * from {}".format(ui)
+           curs.execute(d)
+           t = tuple(curs.fetchall())
+           
+           if t!=():
+              
+            d11 = {
+               'yo' : list(t)}
+
+
+            return render(request,'cart.html',d11) 
+             
+           else:
+            return render(request,'ccart.html') 
+           return render(request,'cart.html',d11) 
+
+
+
+# def  addp(request):
+#    if request.method=="POST":
+#      m = sql.connect(host="localhost",user="root",password="26papapapa",database="dmt")
+#      curs=m.cursor()
+#      d=request.POST
+#      for key,value in d.items():
+#            if key == "tt" :
+#               cun = value
+#      curs.execute("insert into {} values('{}')".format(ui,cun))
+#      m.commit()
+#      return render(request,'ccart.html')           
+
+#      return render(request,'ccart.html')           
+
+
+def display(request):
+   
+     d = "select * from menu"
+     curs.execute(d)
+     t = tuple(curs.fetchall())
+     d11 = {
+               'yy' : list(t)}
+     return render(request,'display.html',d11)
+
+def remove(request):
+              if request.method=="POST":
+               d=request.POST
+               for key,value in d.items():
+                if key == "tt" :
+                 cu = value
+                if key == "pp" :
+                 pr = value
+               curs.execute("delete from {} where id = '{}' AND pr = '{}'".format(ui,cu,pr))
+               m.commit()
+              return redirect('/cart')
